acb4334680603d3fe49e5770d3bf17cb8d35f869
sanket2498 <sanketdulange24@gmail.com>
2021-03-10 12:17:54 +0530

    [sanket]Added : Send reminder email using celery and Test Cases

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,5 +1,7 @@
 from django.core.mail import EmailMessage
 from celery import shared_task
+from notes.models import Notes
+from django.contrib.auth.models import User
 
 
 class Util:
@@ -8,3 +10,16 @@ class Util:
         email = EmailMessage(
             subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
         email.send()
+
+    @shared_task
+    def send_reminder_email(email):
+        print('Send Reminder Email')
+        print(f'Celery Beat{email}')
+        note = Notes.objects.get(owner_id=1)
+        user = User.objects.get(id=1)
+        mail = EmailMessage(
+            subject='Reminder', body='Hi ' + user.username + 'U have a reminder at' + note.reminder, to=user.email)
+        mail.send()
+
+
+
